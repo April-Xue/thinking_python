cc8fa7a03d5af6bf64520733ad504004f830c507
WenhuaXue <wenhuaxue@wenhuaxues-mbp.attlocal.net>
2018-01-01 17:12:46 -0800

    fix

--- ch13.py
+++ ch13.py
@@ -25,3 +25,29 @@ def cleanFile(fname):
 #print cleanFile('../words.txt')[:100]
 
 
+def processLine(line):
+
+	words = line.split()
+	res = []
+
+	for word in words:
+		s = word.translate(string.maketrans("", ""), string.punctuation+string.digits)
+		res.append(s.strip().lower())
+
+	return res 
+
+def processFile(fname, processLine):
+
+	d = dict()
+	with open(fname) as fin:
+		for line in fin:
+			lst = processLine(line)
+			for word in lst:
+				d[word] = d.get(word, 0) + 1
+
+	return d 
+
+
+
+
+
